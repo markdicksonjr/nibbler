a103ecdadeedac42d0329153550239cad257bfce
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-07-13 22:48:07 -0400

    Swapped order of header write in response util helper

--- response.go
+++ response.go
@@ -3,19 +3,19 @@ package nibbler
 import "net/http"
 
 func Write200Json(w http.ResponseWriter, content string) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(content))
 }
 
 func Write404Json(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusNotFound)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
 	w.Write([]byte(`{"result": "not found"}`))
 }
 
 func Write500Json(w http.ResponseWriter, message string) {
-	w.WriteHeader(http.StatusInternalServerError)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
 	w.Write([]byte(`{"result": "` + message + `"}`))
 }
